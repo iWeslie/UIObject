0b73733bca0a349f264bbca486f01ce441c27b8e
Weslie <iweslie@vip.qq.com>
2017-09-21 21:33:03 +0800

    Update UIButton

--- Button/Button/ViewController.swift
+++ Button/Button/ViewController.swift
@@ -9,46 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     
-    @IBOutlet weak var btn1: UIButton!
-    @IBOutlet weak var btn2: UIButton!
+    @IBAction func doSomething() {
+        
+    }
+    
+    @IBAction func doSomething(sender: UIButton) {
+        
+    }
+    
+    @IBAction func doSomething(sender: UIButton, forEvent event: UIEvent) {
+        print(event)
+    }
+    
+    @IBOutlet weak var defaultState: UIButton!
+    @IBOutlet weak var highlightedState: UIButton!
+    @IBOutlet weak var selectedState: UIButton!
+    @IBOutlet weak var disabledState: UIButton!
+    
+    @IBOutlet weak var infolight: UIButton!
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.btn1.setTitle("application", for: .application)
-        self.btn1.setTitle("disabled", for: .disabled)
-        self.btn1.setTitle("focused", for: .focused)
-        self.btn1.setTitle("highlighted", for: .highlighted)
-        self.btn1.setTitle("normal", for: .normal)
-        self.btn1.setTitle("reserved", for: .reserved)
-        self.btn1.setTitle("selected", for: .selected)
-        self.btn1.addTarget(self, action: #selector(btn1Tap), for: .touchUpInside)
-
-        self.btn2.setTitle("application", for: .application)
-        self.btn2.setTitle("disabled", for: .disabled)
-        self.btn2.setTitle("focused", for: .focused)
-        self.btn2.setTitle("highlighted", for: .highlighted)
-        self.btn2.setTitle("normal", for: .normal)
-        self.btn2.setTitle("reserved", for: .reserved)
-        self.btn2.setTitle("selected", for: .selected)
-        self.btn2.addTarget(self, action: #selector(btn2Tap), for: .touchUpInside)
+        
+        //the button is hlighted when clicked
+        highlightedState.isHighlighted = true
+        selectedState.isSelected = true
+        disabledState.isEnabled = false
+        
+        
+//        To configure the appearance of all buttons in your app, use the appearance proxy object
+//        UIButton.appearance().backgroundColor = UIColor.red
+        
+//        self.btn1.setTitle("application", for: .application)
+//        self.btn1.setTitle("disabled", for: .disabled)
+//        self.btn1.setTitle("focused", for: .focused)
+//        self.btn1.setTitle("highlighted", for: .highlighted)
+//        self.btn1.setTitle("normal", for: .normal)
+//        self.btn1.setTitle("reserved", for: .reserved)
+//        self.btn1.setTitle("selected", for: .selected)
+//        self.btn1.addTarget(self, action: #selector(btn1Tap), for: .touchUpInside)
+//
+//        self.btn2.setTitle("application", for: .application)
+//        self.btn2.setTitle("disabled", for: .disabled)
+//        self.btn2.setTitle("focused", for: .focused)
+//        self.btn2.setTitle("highlighted", for: .highlighted)
+//        self.btn2.setTitle("normal", for: .normal)
+//        self.btn2.setTitle("reserved", for: .reserved)
+//        self.btn2.setTitle("selected", for: .selected)
+//        self.btn2.addTarget(self, action: #selector(btn2Tap), for: .touchUpInside)
     }
     
     
-    @objc func btn1Tap() {
-        if btn2.isSelected == true {
-            btn2.isSelected = false
-        }
-        btn1.isSelected = true
-    }
-
-    @objc func btn2Tap() {
-        if btn1.isSelected == true {
-            btn1.isSelected = false
-        }
-        btn2.isSelected = true
-    }
+//    @objc func btn1Tap() {
+//        if btn2.isSelected == true {
+//            btn2.isSelected = false
+//        }
+//        btn1.isSelected = true
+//    }
+//
+//    @objc func btn2Tap() {
+//        if btn1.isSelected == true {
+//            btn1.isSelected = false
+//        }
+//        btn2.isSelected = true
+//    }
 
 }
+
+
+
+
 
